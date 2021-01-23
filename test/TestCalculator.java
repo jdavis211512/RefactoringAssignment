fdb9fc2af0d4a0269ed518f04eb936029800518e
jdavis211512 <Jwdavis2@bsu.edu>
2021-01-23 15:17:03 -0500

    Happy End

--- test/TestCalculator.java
+++ test/TestCalculator.java
@@ -7,27 +7,27 @@ public class TestCalculator {
 
     @Test
     public void Test0(){
-        double testValue = InsuranceCalculator.calculateInsurance(0);
+        double testValue = insuranceCalculator.calculateInsurance(0);
         assertEquals(0,testValue);
     }
     @Test
     public void TestLessThan10000(){
-        double testValue = InsuranceCalculator.calculateInsurance(10000);
+        double testValue = insuranceCalculator.calculateInsurance(0);
         assertEquals(3650,testValue);
     }
     @Test
     public void TestLessthan30000(){
-        double testValue = InsuranceCalculator.calculateInsurance(30000);
+        double testValue = insuranceCalculator.calculateInsurance(0);
         assertEquals(39600,testValue);
     }
     @Test
     public void TestLessthan60000(){
-        double testValue = InsuranceCalculator.calculateInsurance(60000);
+        double testValue = insuranceCalculator.calculateInsurance(0);
         assertEquals(79500,testValue);
     }
     @Test
     public void TestMoreThan60000(){
-        double testValue = InsuranceCalculator.calculateInsurance(90000);
+        double testValue = insuranceCalculator.calculateInsurance(0);
         assertEquals(106200,testValue);
     }
 }
